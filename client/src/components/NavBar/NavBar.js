d32fadb7520c8fdb30f4a835ae32b5131bc1d621
Zach Asher <zach.asher@hotmail.co.uk>
2023-07-26 02:23:13 +0300

    Header image

--- client/src/components/NavBar/NavBar.js
+++ client/src/components/NavBar/NavBar.js
@@ -1,10 +1,31 @@
 import React from 'react'
 import "./nav-bar.scss";
+import { useEffect, useState } from 'react';
 
 
 function NavBar() {
+  const [navbarClass, setNavbarClass] = useState('navbar navbar-transparent');
+
+  useEffect(() => {
+    window.addEventListener('scroll', handleScroll);
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, []);
+
+  const handleScroll = () => {
+    const scrollPosition = window.scrollY;
+
+    // Update the class name based on the scroll position
+    if (scrollPosition > 100) {
+      setNavbarClass('navbar navbar-solid');
+    } else {
+      setNavbarClass('navbar navbar-transparent');
+    }
+  };
+
   return (
-    <div className='navbar'>
+    <div className={navbarClass}>
         <p className='navbar__logo'>AF</p>
         <div className='navbar__links'>
             <p className='navbar__link'>CLASSES</p>
